353a5eefe8888cbf0b311002543fccbe344bbfa7
mathvaillant <matheusvaillant505@gmail.com>
2021-05-26 17:02:55 +0100

    scheduler and whatsapp messagim done

--- src/components/Scheduler/Scheduler.js
+++ src/components/Scheduler/Scheduler.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import './Scheduler.scss'
 import Barb from '../../images/mustache.png'
-import Upload from '../../images/upload.png'
 import WhatsApp from '../../images/whatsapp.png'
 import DateTimePicker from 'react-datetime-picker'
 
@@ -9,8 +8,9 @@ const Scheduler = () => {
   const [date, setDate] = useState(new Date())
   const [time, setTime] = useState('')
   const [day, setDay] = useState('')
-  const [name, setName] = useState('Default name')
+  const [name, setName] = useState('')
   const [address, setAddress] = useState('')
+
   const onChange = (date) => {
     if (date !== null) {
       const fullDate = date.toLocaleDateString()
@@ -24,16 +24,16 @@ const Scheduler = () => {
     }
   }
 
-  const whatsAppHandler = () => {
+  const whatsAppHandler = (e) => {
+    e.preventDefault()
     if (time === '' || day === '' || name === '') {
       alert('Informe data, hora e seu nome')
     } else {
       let newTime = time
       let newDay = day
-      let newAddress = 'Endereço aleatório'
+      let newAddress = address
       let clientName = name
-      let sampleImg = ''
-      let message = `Olá Samuel, como está? Gostava de agendar um corte para o dia ${newDay} às ${newTime} em minha residência que fica no endereço ${newAddress}. Em anexo deixo o estilo que mais curto para meu cabelo. Fico a aguardar uma confirmação, Obrigado! ${clientName}`
+      let message = `Olá Samuel, como está? Gostava de agendar um corte para o dia ${newDay} às ${newTime} ${newAddress}. Fico a aguardar uma confirmação, Obrigado! ${clientName}`
       const location = window.location
       location.replace(
         `https://api.whatsapp.com/send?phone=+351963007246&text=${message}&source=&data=&app_absent=`
@@ -46,30 +46,42 @@ const Scheduler = () => {
       <h1>Agendamentos</h1>
       <img src={Barb} alt='bar.png' />
       <div className='scheduler__inner'>
-        <h4>Selecione a melhor data e horário </h4>
-        <br />
-        <DateTimePicker
-          minDate={new Date()}
-          className='datePicker'
-          onChange={onChange}
-          value={date}></DateTimePicker>
-        <h4>Qual seu estilo de corte favorito? (Opcional)</h4>
-        <br />
         <form>
-          <img src={Upload} alt='' />
-          <input type='file' name='file' id='file' />
-        </form>
-        <br />
-        <h4>
-          Tem alguma alergia? <input type='text' />
-        </h4>
-        <h4>Observações</h4>
-        <br />
-        <textarea name='message' id='message' cols='30' rows='3'></textarea>
+          <label htmlFor='name'>Diga-nos o seu nome</label>
+          <input
+            type='text'
+            id='name'
+            name='name'
+            onChange={(e) => setName(e.target.value)}
+          />
 
-        <button className='book' onClick={whatsAppHandler}>
-          Agendar pelo WhatsApp <img src={WhatsApp} alt='' />
-        </button>
+          <h4>Selecione a melhor data e horário </h4>
+
+          <DateTimePicker
+            minDate={new Date()}
+            className='datePicker'
+            onChange={onChange}
+            value={date}></DateTimePicker>
+
+          <label htmlFor='where'>Onde prefere cortar?</label>
+          <select
+            name='where'
+            id='where'
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}>
+            <option value='Minha casa'>Em minha residência</option>
+            <option value='Casa do Samuel'>Residência do Barbeiro</option>
+          </select>
+
+          <label htmlFor='message'>Observações</label>
+
+          <textarea name='message' id='message' cols='30' rows='3'></textarea>
+          <br />
+          <button className='book' onClick={whatsAppHandler}>
+            Agendar
+            <img src={WhatsApp} alt='' className='WhatsAppimg' />
+          </button>
+        </form>
       </div>
     </div>
   )
