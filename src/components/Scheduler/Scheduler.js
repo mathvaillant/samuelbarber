d057b535ba41f504a8ed30f69789528caec1019f
mathvaillant <matheusvaillant505@gmail.com>
2021-05-26 12:10:21 +0100

    whatsapp redirect built

--- src/components/Scheduler/Scheduler.js
+++ src/components/Scheduler/Scheduler.js
@@ -2,30 +2,43 @@ import React, { useState } from 'react'
 import './Scheduler.scss'
 import Barb from '../../images/mustache.png'
 import Upload from '../../images/upload.png'
+import WhatsApp from '../../images/whatsapp.png'
 import DateTimePicker from 'react-datetime-picker'
 
 const Scheduler = () => {
   const [date, setDate] = useState(new Date())
-  const [scheduleDate, setScheduleDate] = useState('')
-  const [chatMessage, setChatMessage] = useState('')
-  const [chatOpen, setChatOpen] = useState(false)
-
+  const [time, setTime] = useState('')
+  const [day, setDay] = useState('')
+  const [name, setName] = useState('Default name')
+  const [address, setAddress] = useState('')
   const onChange = (date) => {
     if (date !== null) {
-      const fullDate = date.toDateString()
+      const fullDate = date.toLocaleDateString()
       const fullTime = date.getHours()
       const fullMinutes = date.getMinutes()
       setDate(date)
-      setScheduleDate(
-        `${fullDate} | Às ${fullTime.toString()}:${fullMinutes.toString()}`
-      )
+      setDay(fullDate.toString())
+      setTime(`${fullTime.toString()}:${fullMinutes.toString()}`)
     } else {
       return null
     }
   }
 
-  const openChatHandler = () => {
-    setChatOpen(true)
+  const whatsAppHandler = () => {
+    if (time === '' || day === '' || name === '') {
+      alert('Informe data, hora e seu nome')
+    } else {
+      let newTime = time
+      let newDay = day
+      let newAddress = 'Endereço aleatório'
+      let clientName = name
+      let sampleImg = ''
+      let message = `Olá Samuel, como está? Gostava de agendar um corte para o dia ${newDay} às ${newTime} em minha residência que fica no endereço ${newAddress}. Em anexo deixo o estilo que mais curto para meu cabelo. Fico a aguardar uma confirmação, Obrigado! ${clientName}`
+      const location = window.location
+      location.replace(
+        `https://api.whatsapp.com/send?phone=+351963007246&text=${message}&source=&data=&app_absent=`
+      )
+    }
   }
 
   return (
@@ -53,8 +66,9 @@ const Scheduler = () => {
         <h4>Observações</h4>
         <br />
         <textarea name='message' id='message' cols='30' rows='3'></textarea>
-        <button className='book' onClick={openChatHandler}>
-          Agendar
+
+        <button className='book' onClick={whatsAppHandler}>
+          Agendar pelo WhatsApp <img src={WhatsApp} alt='' />
         </button>
       </div>
     </div>
